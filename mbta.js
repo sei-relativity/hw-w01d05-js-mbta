c75a45ee990634d84bd3a2ec5d773037082971bc
Sager Alarifi <sagr.1993@gmail.com>
2019-10-04 03:28:00 +0300

    initialize values

---
+++ mbta.js
@@ -0,0 +1,14 @@
+//  Sager Alarifi
+//  SEI - hw-w01d05
+//  Main function
+const stopsBetweenStations = function (sLine, sStation, eLine, eStation) {
+    //  Declaring and defining lines as an array, and stops are strings in that array
+    const redLine = ["South Station", "Park Street", "Kendall", "Central", "Harvard",
+        "Porter", "Davis", "Alewife"];
+    const greenLine = ["Government Center", "Park Street", "Boylston", "Arlington",
+        "Copley", "Hynes", "Kenmore"]
+    const orangeLine = ["North Station", "Haymarket", "Park Street", "State",
+        "Downtown Crossing", "Chinatown", "Back Bay", "Forest Hills"]
+
+    
+}
