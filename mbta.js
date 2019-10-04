16a17f3fc137dd4b8dcac5cced7bcb81db1697bf
Sager Alarifi <sagr.1993@gmail.com>
2019-10-04 19:06:30 +0300

    finish all, line index bug left

--- mbta.js
+++ mbta.js
@@ -1,5 +1,6 @@
 //  Sager Alarifi
 //  SEI - hw-w01d05
+
 //  Main function
 const stopsBetweenStations = function (sLine, sStation, eLine, eStation) {
     //  Declaring and defining lines as an array, and stops are strings in that array
@@ -10,5 +11,38 @@ const stopsBetweenStations = function (sLine, sStation, eLine, eStation) {
     const orangeLine = ["North Station", "Haymarket", "Park Street", "State",
         "Downtown Crossing", "Chinatown", "Back Bay", "Forest Hills"]
 
+    let indexS; //  index of the start station
+    let indexE; //  index of the end station
+
+    //  logic to get the index of the user input
+    //  Starting station
+    if(sLine == "Red")
+    indexS = redLine.indexOf(sStation);
+    if(sLine == "Green")
+    indexS = greenLine.indexOf(sStation);
+    if(sLine == "Orange")
+    indexS = orangeLine.indexOf(sStation);
     
+    //  Ending Station
+    if(eLine == "Red")
+    indexE = redLine.indexOf(eStation);
+    if(eLine == "Green")
+    indexE = greenLine.indexOf(eStation);
+    if(eLine == "Orange")
+    indexE = orangeLine.indexOf(eStation);
+    
+    //  initializing number of stops
+    let stopsNum = 0;
+    //  case passenger on same line
+    if(eLine == sLine){
+        stopsNum = 0;
+        stopsNum = Math.abs(indexE - indexS);
+    } else{
+        stopsNum = 0;
+        stopsNum += Math.abs(sLine.indexOf("Park Street") - indexS)
+        stopsNum += Math.abs(eLine.indexOf("Park Street") - indexE);
+    }
+
+    //  print out the number of stops
+    console.log("" + sLine.indexOf("Park Street") + " stops");
 }
