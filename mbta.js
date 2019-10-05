df2a7234cf9903426a73b9370bb0da878cca73e5
Mohammed <Mohammed.ofisan@hotmail.com>
2019-10-05 23:57:01 +0300

    Mohammed Alofaysan

---
+++ mbta.js
@@ -0,0 +1,14 @@
+const subway = {
+    Red:["South Station","Park Street","Kendall","Central","Harvard","Porter","Davis","Alewife"],
+    Green:["Government Center","Park Street","Boylston","Arlington","Copley","Hynes","Kenmore"],
+    Orange:["North Station","Haymarket","Park Street","State","Downtown Crossing","Chinatown","Back Bay","Forest Hills"]
+}
+const stopsBetweenStations =function(startLain,endLine,startStation,endStation){
+    let x = endLine = 0;
+for(let i =0; i < subway[i].length;i++){
+x +=subway[i];
+
+return x;
+}
+}
+
