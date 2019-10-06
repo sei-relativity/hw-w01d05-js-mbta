ba04a1a46b68bdeec4e1ff7846a187abb048560a
Sager Alarifi <sagr.1993@gmail.com>
2019-10-06 08:18:59 +0300

    turned arrays into objects and handle errors

--- mbta.js
+++ mbta.js
@@ -3,53 +3,50 @@
 
 //  Main function
 const stopsBetweenStations = function (sLine, sStation, eLine, eStation) {
-    //  Declaring and defining lines as an array, and stops are strings in that array
-    const redLine = ["South Station", "Park Street", "Kendall", "Central", "Harvard",
-        "Porter", "Davis", "Alewife"];
-    const greenLine = ["Government Center", "Park Street", "Boylston", "Arlington",
-        "Copley", "Hynes", "Kenmore"]
-    const orangeLine = ["North Station", "Haymarket", "Park Street", "State",
-        "Downtown Crossing", "Chinatown", "Back Bay", "Forest Hills"]
-    
-    let indexS; //  index of the start station
-    let indexE; //  index of the end station
+    //  Declaring and defining lines as an object of arrays, and stops are strings in that array
+    const linesArr = {
+        Red: ["South Station", "Park Street", "Kendall", "Central", "Harvard",
+            "Porter", "Davis", "Alewife"],
+        Green: ["Government Center", "Park Street", "Boylston", "Arlington",
+            "Copley", "Hynes", "Kenmore"],
+        Orange: ["North Station", "Haymarket", "Park Street", "State",
+            "Downtown Crossing", "Chinatown", "Back Bay", "Forest Hills"]
+    }
 
-    //  logic to get the index of the user input
-    //  Starting station
-    if(sLine == "Red")
-    indexS = redLine.indexOf(sStation);
-    if(sLine == "Green")
-    indexS = greenLine.indexOf(sStation);
-    if(sLine == "Orange")
-    indexS = orangeLine.indexOf(sStation);
-    
-    //  Ending Station
-    if(eLine == "Red")
-    indexE = redLine.indexOf(eStation);
-    if(eLine == "Green")
-    indexE = greenLine.indexOf(eStation);
-    if(eLine == "Orange")
-    indexE = orangeLine.indexOf(eStation);
-    
+    //  Handeling wrong line input
+    if (sLine != "Red" && sLine != "Green" && sLine != "Orange") {
+        console.log("invalid starting line input");
+        return
+    }
+    if (eLine != "Red" && eLine != "Green" && eLine != "Orange") {
+        console.log("invalid ending line input");
+        return
+    }
+
+    //  build arrays for input
+    const sArr = linesArr[sLine];
+    const eArr = linesArr[eLine];
+
+    //  handling for wrong station input
+    if(sArr.indexOf(sStation)== -1){
+        console.log("invalid starting station input");
+        return
+    }
+
+    if(eArr.indexOf(eStation)== -1){
+        console.log("invalid ending station input");
+        return
+    }
+
     //  initializing number of stops
     let stopsNum = 0;
-    //  case passenger on same line
-    if(eLine == sLine){
-        stopsNum = Math.abs(indexE - indexS);
-    } else{
-        if(sLine == "Red")
-        stopsNum += Math.abs(redLine.indexOf("Park Street") - indexS)
-        if(sLine == "Green")
-        stopsNum += Math.abs(greenLine.indexOf("Park Street") - indexS)
-        if(sLine == "Orange")
-        stopsNum += Math.abs(orangeLine.indexOf("Park Street") - indexS)
 
-        if(eLine == "Red")
-        stopsNum += Math.abs(redLine.indexOf("Park Street") - indexE)
-        if(eLine == "Green")
-        stopsNum += Math.abs(greenLine.indexOf("Park Street") - indexE)
-        if(eLine == "Orange")
-        stopsNum += Math.abs(orangeLine.indexOf("Park Street") - indexE)
+    //  case passenger on same line
+    if (eLine == sLine) {
+        stopsNum = Math.abs(eArr.indexOf(eStation) - sArr.indexOf(sStation));
+    } else {
+            stopsNum += Math.abs(sArr.indexOf("Park Street") - sArr.indexOf(sStation));
+            stopsNum += Math.abs(eArr.indexOf("Park Street") - eArr.indexOf(eStation));
     }
 
     //  print out the number of stops
