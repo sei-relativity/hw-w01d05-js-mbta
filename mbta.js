9d2665c2c6b9f72d44a5942c127f72965be797cc
obal3588 <obal3588@colorado.edu>
2019-10-06 11:15:18 +0300

    using one obj instead of three

--- mbta.js
+++ mbta.js
@@ -1,23 +1,28 @@
 
+
+
+
 const stopsBetweenStations = function (Start_Line, Start_Station, End_Line, End_Station
 
     ){
-    const Red ={
-        Path:['South Station','Park Street','Kendall' ,'Central',"Harvard","Porter","Davis","Alewife"]
+    const Subway ={
+         "Red":['South Station','Park Street','Kendall' ,'Central',"Harvard","Porter","Davis","Alewife"],
+         "Green":["Government Center","Park Street","Boylston","Arlington","Copley","Hynes","Kenmore"],
+         "Orange": ["North Station","Haymarket","Park Street","State","Downtown Crossing","Chinatown","Back Bay","Forest Hills"]
+
     }
-    const  Green ={
-       Path: ["Government Center","Park Street","Boylston","Arlington","Copley","Hynes","Kenmore"]
-    }
-    const Orange={
-      Path:  ["North Station","Haymarket","Park Street","State","Downtown Crossing","Chinatown","Back Bay","Forest Hills"]
-    }
-    const three_road=[Red,Green,Orange];
+   
+    
+        if (!( Object.getOwnPropertyNames(Subway).includes(Start_Line)) || !( Object.getOwnPropertyNames(Subway).includes(End_Line)) ){
+
+                //debugger;
+                return "invalid Road"}
+        else if  (!(Subway[Start_Line].includes(Start_Station))  || !( Subway[End_Line].includes(End_Station)) )
+            return "invalid Station"
+
+
 
     let state ="null";
-    let stops=null;
-    let start_point;
-    let end_point ;
-
     if (Start_Line===End_Line){
         if (Start_Station===End_Station)
             state = "in_place"
@@ -29,28 +34,36 @@ const stopsBetweenStations = function (Start_Line, Start_Station, End_Line, End_
         state ="two_line" 
     }
 
+
+    /*Start calculate stops basit on state */
+
+    let stops=null;
+    let start_point;
+    let end_point ;
+
+
         switch(state){
 
             case "same_line":
                 {
-                    start_point=three_road[three_road.indexOf(eval(Start_Line))].Path.indexOf(Start_Station)  ;   
-                    end_point=three_road[three_road.indexOf(eval(End_Line))].Path.indexOf(End_Station)  ;      
+                    start_point=Subway[Start_Line].indexOf(Start_Station)  ;   
+                    end_point=Subway[End_Line].indexOf(End_Station) ;      
                     return Math.abs(start_point-end_point)
                 }
             case"in_place" :
                     return 0;
 
             case "two_line" :{
-
+                    
                 let temp1,temp2;
-                start_point=three_road[three_road.indexOf(eval(Start_Line))].Path.indexOf(Start_Station);   
-                end_point=three_road[three_road.indexOf(eval(Start_Line))].Path.indexOf("Park Street");   
+                start_point=Subway[Start_Line].indexOf(Start_Station);   
+                end_point=Subway[Start_Line].indexOf("Park Street");   
                 
-                temp1 = three_road[three_road.indexOf(eval(End_Line))].Path.indexOf(End_Station);  
-                temp2 = three_road[three_road.indexOf(eval(End_Line))].Path.indexOf("Park Street");  
+                temp1 = Subway[End_Line].indexOf(End_Station);  
+                temp2 = Subway[End_Line].indexOf("Park Street");  
                 
                 stops = Math.abs(start_point-end_point) + Math.abs(temp1-temp2);
-                debugger;
+               // debugger;
                 return stops
             }
 
