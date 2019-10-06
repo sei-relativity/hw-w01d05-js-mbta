c18c7367175ab3dceea67c3f8be3ddad3e79ef62
wahhab1404 <a.wahhab1404@gmail.com>
2019-10-06 07:21:50 +0300

    sub-homework

---
+++ mbta.js
@@ -0,0 +1,19 @@
+//2
+const numArry = [1,2,3,4];
+const evenNumArry = [];
+
+undefined
+for(i= 0;i < numArry.length; i++) {
+if (numArry[i]%2 ==0){
+	evenNumArry.push(numArry[i])
+	}
+}
+for (let x = -1; x < evenNumArry -1; x++) {
+    total = evenNumArry[x] + evenNumArry[x+1]
+    }
+    console.log(total);
+
+//5
+const addDash = ["test1","test2","3"];
+addDash.join("_");
+return addDash
