659cd100d17b41de967569e98d0fe67861df6c11
aba20 <askr1000@gmail.com>
2019-10-07 05:20:19 +0300

    Abdulrahman Alfouzan

--- mbta.js
+++ mbta.js
@@ -1,10 +1,30 @@
-const subwayLines = {
-    red: [ 'South Station', 'Park Street', 'Kendall', 'Central', 'Harvard', 'Porter', 'Davis', 'Alewife'],
-    green: [ 'Government Center', 'Park Street', 'Boylston', 'Arlington', 'Copley', 'Hynes', 'Kenmore'],
-    orange: [ 'North Station', 'Haymarket', 'Park Street', 'State', 'Downtown', 'Chinatown', 'Back Bay', 'Forest Hills']
-}
 
 const stopBetweenStation = function(startLine, startStation, endLine, endStation){
+
+    const lines= {
+        red: [ 'South Station', 'Park Street', 'Kendall', 'Central', 'Harvard', 'Porter', 'Davis', 'Alewife'],
+        green: [ 'Government Center', 'Park Street', 'Boylston', 'Arlington', 'Copley', 'Hynes', 'Kenmore'],
+        orange: [ 'North Station', 'Haymarket', 'Park Street', 'State', 'Downtown', 'Chinatown', 'Back Bay', 'Forest Hills'],
+    },
+    
+        if(startLine === endLine){
+        let indexOfStartStation = lines[startLine].indexOf(startStation);
+        let indexOfEndStation = lines[startLine].indexOf(endStation);
+        let stops = indexOfStartStation - indexOfEndStation
+        console.log(Math.abs(stops))
+        }
+
+        if(startLine !== endLine){
+            let indexOfStartStation = lines[startLine].indexOf('Park Street');
+            let indexOfEndStation = lines[endtLine].indexOf('Park Street');
+            let stops = indexOfEndStation - indexOfStartStation
+        }
+    }
+
+    stopBetweenStation('red', 'Kendall', 'red', "Porter")
+// (red, kendall, red, Central)
+// }
+/*const stopBetweenStation = function(startLine, startStation, endLine, endStation){
     let countr = 0
     for (let i=0; i <= startLine.length; i++){
         countr += startLine[i]
@@ -35,7 +55,7 @@ const stopBetweenStation = function(startLine, startStation, endLine, endStation
     };
 
 };
+}
 
 
-
-console.log(stopBetweenStation(green, Hynes, red, Central))
+console.log(stopBetweenStation(green, Hynes, red, Central))*/
