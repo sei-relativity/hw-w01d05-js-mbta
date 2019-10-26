aff6dec45b238d5b83dbfd5f8ba00074fc2424de
wahhab1404 <a.wahhab1404@gmail.com>
2019-10-26 15:13:18 +0300

    abdlwahhab

--- mbta.js
+++ mbta.js
@@ -1,19 +1,36 @@
-//2
-const numArry = [1,2,3,4];
-const evenNumArry = [];
-
-undefined
-for(i= 0;i < numArry.length; i++) {
-if (numArry[i]%2 ==0){
-	evenNumArry.push(numArry[i])
-	}
+const subwayLines = {
+    Red: ["South Station", "Park Street", "Kendall", "Central", "Harvard", "Porter", "Davis","Alewife"],
+    Green: ["Government Center", "Park Streen", "Boylston", "Arlington", "Copley", "Hynes", "Kenmore"],
+    Orange: ["North Station", "Haymarket", "Park Street", "State", "Downtown Crossing", "Chinatown","Back Bay", "Forest Hills"]
 }
-for (let x = -1; x < evenNumArry -1; x++) {
-    total = evenNumArry[x] + evenNumArry[x+1]
+const check = function(line, station){
+    for(const key in subwayLines){
+        if(key == line){
+            for(i =0; i<subwayLines[key].length; i++){
+                if(station == subwayLines[key][i]) return true
+            }
+        }
     }
-    console.log(total);
+    return false
+}
 
-//5
-const addDash = ["test1","test2","3"];
-addDash.join("_");
-return addDash
+const stopsBetweenStations = function (startLine, startStaion, endLin, endStaion){
+    let count = 0;
+    let toInter1 = 0;
+    let toInter2 = 0;
+    if(startLine == endLine){
+        count = Math.abs(subwayLines[startLine].indexOf(startStaion) - subwayLines[endLine].indexOf(endStaion))
+        return count
+    }
+    else {
+        toInter1 = Math.abs(subwayLines[endLine].indexOf(endStaion)-subwayLines[endLine].indexOf("Park Street"))
+        toInter2 = Math.abs(subwayLines[startLine].indexOf(startStaion)-subwayLines[startLine].indexOf("Park Street"))
+        count = Math.abs(toInter2-toInter1)
+    }
+    return count
+}
+console.log(stopsBetweenStations("Red", "Alewife", "Red", "South Station"))
+console.log(stopsBetweenStations('Red', 'Alewife', 'Red', 'Alewife'))
+console.log(stopsBetweenStations('Red', 'South Station', 'Red', 'Alewife'))
+console.log(stopsBetweenStations('Red', 'South Station', 'Green', 'Kenmore'))
+console.log(stopsBetweenStations('Green', 'Boylston', 'Orange', 'State')) 
--- pull_request_template.md
+++ pull_request_template.md
@@ -2,9 +2,9 @@
 
 ---
 
-##### How difficult did you find this (0 - 10)? (0 very easy, 10 very difficult)
-##### How much of this did you complete? (0-10)
-##### How long did it take?
+##### How difficult did you find this (0 - 10)? (0 very easy, 10 very difficult)8
+##### How much of this did you complete? (0-10)7
+##### How long did it take? 2 hours
 ##### Was there anything that you struggled with?  If so, what?
-##### Is there anything that you would like some further information on?
-##### Do you have any suggestions to improve this assignment?
+##### Is there anything that you would like some further information on?no
+##### Do you have any suggestions to improve this assignment?no
